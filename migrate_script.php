4929c04c404ab15c5bc38a863cabc782982d6db8
root <root@localhost>
2025-03-20 10:04:31 +0100

    Primer commit

---
+++ migrate_script.php
@@ -0,0 +1,57 @@
+#!/usr/bin/php
+<?php
+
+$directory = getcwd() . "\src\\";
+
+function isUseOfCommonServiceStatement($line): bool
+{
+    return strpos($line, 'Admin\Library\Services') && !strpos($line, 'Admin\Library\Services\Commons');
+}
+
+function editFile($file): void
+{
+    $lines = file($file);
+    foreach ($lines as $key => $line) {
+        if (isUseOfCommonServiceStatement($line)) {
+            $newLine = str_replace(
+                'Admin\Library\Services\\',
+                'Admin\Library\Services\Commons\\',
+                $line
+            );
+            $lines[$key] = $newLine;
+        }
+        if (strpos($line, 'class')) {
+            break;
+        }
+    }
+
+    $fileWrite = fopen($file, 'wb');
+    foreach ($lines as $line) {
+        fwrite($fileWrite, $line);
+    }
+
+    fclose($fileWrite);
+}
+
+function modifyFiles($directory) {
+    $dir = opendir($directory);
+    while ($element = readdir($dir)){
+        if( $element !== "." && $element !== ".."){
+            $path = $directory.$element;
+            if(
+                is_dir($path) && !strpos($path, 'Entity') &&
+                !strpos($path, 'Migrations') &&
+                !strpos($path, 'Security') &&
+                !strpos($path, 'Front') &&
+                !strpos($path, 'Form') &&
+                !strpos($path, 'Command')
+            ){
+                modifyFiles($path . '/');
+            } elseif (strpos($path, '.php') && !strpos($path, 'Kernel')) {
+                editFile($path);
+            }
+        }
+    }
+}
+
+modifyFiles($directory);
---
+++ src/Controller/Front/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="front_index", methods={"GET"})
+     */
+
+    public function index(): Response
+    {
+
+        return $this->render('base.html.twig');
+
+    }
+
+
+}
